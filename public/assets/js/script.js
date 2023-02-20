e72e7b80bbf6a32a7dec1416a9e0085c71b2d657
Tanish Panjwani <tanish@paywise.io>
2023-02-20 12:23:57 +0300

    updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,75 +1,73 @@
-const applePayButton = document.querySelector("apple-pay-button");
+const applePayButton = document.querySelector('apple-pay-button')
 
-applePayButton.addEventListener("click", async (e) => {
-  e.preventDefault();
-  onApplePayButtonClicked();
-});
+applePayButton.addEventListener('click', async (e) => {
+  e.preventDefault()
+  onApplePayButtonClicked()
+})
 
 function onApplePayButtonClicked() {
   if (!ApplePaySession) {
-    return;
+    return
   }
 
   const request = {
-    countryCode: "QA",
-    currencyCode: "QAR",
-    merchantCapabilities: ["supports3DS"],
-    supportedNetworks: ["visa", "masterCard"],
+    countryCode: 'QA',
+    currencyCode: 'QAR',
+    merchantCapabilities: ['supports3DS'],
+    supportedNetworks: ['visa', 'masterCard'],
     total: {
-      label: "Insurance",
-      type: "final",
-      amount: "50",
+      label: 'Safe Insurance Co',
+      type: 'final',
+      amount: '1',
     },
-  };
+  }
 
-  const session = new ApplePaySession(3, request);
+  const session = new ApplePaySession(3, request)
 
   // Open the payment sheet
   session.onvalidatemerchant = (event) => {
-    
-
     // Fetch the merchant session to validate the payment request
-    fetch("/payments/applepay/session", {
-      method: "POST",
+    fetch('/payments/applepay/session', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-      }
+        'Content-Type': 'application/json',
+      },
     })
       .then((res) => res.json()) // Parse response as JSON.
       .then((merchantSession) => {
-        session.completeMerchantValidation(merchantSession);
+        session.completeMerchantValidation(merchantSession)
       })
       .catch((err) => {
-        console.error("Error fetching merchant session", err);
-      });
-  };
+        console.error('Error fetching merchant session', err)
+      })
+  }
 
   // Process token and respond to customer
   session.onpaymentauthorized = async (event) => {
-    var isSuccess;
+    var isSuccess
 
     try {
-      const response = await fetch("/payments/applepay/tokens", {
-        method: "POST",
+      const response = await fetch('/payments/applepay/tokens', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(event.payment.token),
-      });
+      })
       isSuccess = {
         status: 0,
-      };
+      }
     } catch (error) {
       isSuccess = {
         status: 1,
-      };
+      }
     }
-    session.completePayment(isSuccess);
-  };
+    session.completePayment(isSuccess)
+  }
 
   session.oncancel = (event) => {
-    alert("Payment has been cancelled");
-  };
+    alert('Payment has been cancelled')
+  }
 
-  session.begin();
+  session.begin()
 }
--- server.js
+++ server.js
@@ -1,90 +1,87 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
-require("dotenv").config();
+const express = require('express')
+const axios = require('axios')
+const app = express()
+require('dotenv').config()
 
-const PORT = process.env.PORT || 4545;
+const PORT = process.env.PORT || 4545
 
-app.use(express.static("public"));
-app.use(express.json());
+app.use(express.static('public'))
+app.use(express.json())
 
 // CREATE A PAYMENT SESSION
 
-app.post("/payments/applepay/session", async (req, res) => {
+app.post('/payments/applepay/session', async (req, res) => {
   try {
     const options = {
-      "domain": "4379-37-186-50-252.ngrok.io",
-      "validationUrl": "https://apple-pay-gateway.apple.com/paymentservices/paymentSession"
-    };
+      domain: process.env.DOMAIN,
+      validationUrl:
+        'https://apple-pay-gateway.apple.com/paymentservices/paymentSession',
+    }
 
     const response = await axios.post(
       'https://api.dibsy.one/v2/wallets/applepay/session',
       JSON.stringify(options),
       {
         headers: {
-          'Authorization': 'Bearer sk_live_',
-          "Content-Type": "application/json",
-        }
-      }
-    );
+          Authorization: 'Bearer ' + process.env.DIBSY_API_SECRET_KEY,
+          'Content-Type': 'application/json',
+        },
+      },
+    )
 
-    console.log('response', response.data);
+    console.log('response', response.data)
 
-    res.send(response?.data);
-
+    res.send(response?.data)
   } catch (error) {
     console.log(error.response.data)
   }
-});
+})
 
-// PROCESS PAYMENT TOKEN 
+// PROCESS PAYMENT TOKEN
 
-app.post("/payments/applepay/tokens", async (req, res) => {
-
-  const token = req?.body;
+app.post('/payments/applepay/tokens', async (req, res) => {
+  const token = req?.body
   console.log('token', token)
 
   const opts = {
-    "method": "applepay",
-    "amount":{
-      "value": '1.00',
-      "currency": "QAR"
+    method: 'applepay',
+    amount: {
+      value: '1.00',
+      currency: 'QAR',
     },
-    "description": "Test",
-    "redirectUrl": "https://example.com",
-    "applePayToken": JSON.stringify(token),
+    description: 'Order #100001',
+    metadata: {
+      orderId: '100001',
+      customerId: '100',
+      device: 'iOS',
+    },
+    redirectUrl: 'https://example.com',
+    applePayToken: JSON.stringify(token),
   }
 
   try {
-
     const response = await axios.post(
       'https://api.dibsy.one/v2/payments',
       opts,
       {
         headers: {
-          'Authorization': 'Bearer sk_live_',
-          "Content-Type": "application/json",
-        }
-      }
-    );
+          Authorization: 'Bearer ' + process.env.DIBSY_API_SECRET_KEY,
+          'Content-Type': 'application/json',
+        },
+      },
+    )
 
     console.log('response', response.data)
 
-    if (response?.data?.status === "succeeded") {
+    if (response?.data?.status === 'succeeded') {
       res.sendStatus(200)
-    }
-    else {
+    } else {
       res.sendStatus(400)
     }
-    
   } catch (error) {
     console.log('error', error.response.data)
     res.sendStatus(400)
   }
+})
 
-
-});
-
-
-
-app.listen(PORT, () => console.log(`Node server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Node server listening on port ${PORT}`))
